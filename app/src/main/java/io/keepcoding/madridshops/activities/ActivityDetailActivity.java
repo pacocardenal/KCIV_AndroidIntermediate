69b0edb0a7ebf582aba91b0c2909c7da04f4ccb2
Paco Cardenal <paco@pacocardenal.com>
2017-07-06 23:07:32 +0200

    New interface of Activities Detail screen

--- app/src/main/java/io/keepcoding/madridshops/activities/ActivityDetailActivity.java
+++ app/src/main/java/io/keepcoding/madridshops/activities/ActivityDetailActivity.java
@@ -1,15 +1,42 @@
 package io.keepcoding.madridshops.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import io.keepcoding.madridshops.R;
+import io.keepcoding.madridshops.domain.model.Activity;
+import io.keepcoding.madridshops.util.Constants;
 
 public class ActivityDetailActivity extends AppCompatActivity {
+
+    @BindView(R.id.activity_activity_detail__activity_address) TextView address;
+    @BindView(R.id.activity_activity_detail__activity_description) TextView description;
+    @BindView(R.id.activity_activity_detail__activity_image) ImageView activityImage;
+    @BindView(R.id.activity_activity_detail__activity_name) TextView name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity_detail);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            Activity activity = (Activity) intent.getSerializableExtra(Constants.INTENT_ACTIVITY_DETAIL);
+            name.setText(activity.getName());
+            address.setText(activity.getAddress());
+            description.setText(activity.getDescription());
+            Picasso.with(this)
+                    .load(activity.getImageUrl())
+                    .placeholder(R.drawable.shop_placeholder)
+                    .into(activityImage);
+        }
     }
 }
